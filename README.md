0885a538766dfb73977b0ab8b4c81ae7eee7e6d2
Andy Wunderlich <wunderlich@qurdo.com>
2020-03-02 21:44:32 +0100

    Added modulo 26 to crawl only selected urls

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# brickcrawler
+# brickcrawler for brickscout.com
+
+Crawls sitemap and open urls to start prerender for selected caching
+
+Controlled by product id and modulo
+
+created by Andy Wunderlich
--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+/**
+ * @package    BrickCrawler
+ *
+ * @author     Andy Wunderlich <hello@andy-wunderlich.de>
+ * @copyright  Andy Wunderlich
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * @link       https://www.andy-wunderlich.de
+ */
+
 var sitemaps = require('sitemap-stream-parser');
 var Crawler = require("crawler");
+
 var urls = [];
 
 urls.push('https://brickscout.com/sitemaps/shops/de/0.xml');
@@ -7,31 +17,60 @@ urls.push('https://brickscout.com/sitemaps/shops/en/0.xml');
 urls.push('https://brickscout.com/sitemaps/shops/it/0.xml');
 urls.push('https://brickscout.com/sitemaps/shops/nl/0.xml');
 
-for (i = 146; i >= 0; i--) {
-urls.push('https://brickscout.com/sitemaps/products/de/'+i+'.xml');
-urls.push('https://brickscout.com/sitemaps/products/en/'+i+'.xml');
-urls.push('https://brickscout.com/sitemaps/products/it/'+i+'.xml');
-urls.push('https://brickscout.com/sitemaps/products/nl/'+i+'.xml');
+for (i = 154; i >= 0; i--) {
+    urls.push('https://brickscout.com/sitemaps/products/de/'+i+'.xml');
+    urls.push('https://brickscout.com/sitemaps/products/en/'+i+'.xml');
+    urls.push('https://brickscout.com/sitemaps/products/it/'+i+'.xml');
+    urls.push('https://brickscout.com/sitemaps/products/nl/'+i+'.xml');
 }
+
 all_urls = [];
+
 var c = new Crawler({
-maxConnections : 2,
-//rateLimit: 1000, // `maxConnections` will be forced to 1
-userAgent: 'Googlebot/2.1 (+http://www.googlebot.com/bot.html)',
-// This will be called for each crawled page
-callback : function (error, res, done) {
-if(error){
-console.log(error);
-}else{
-//a lean implementation of core jQuery designed specifically for the server
-console.log(res.options.uri);
-}
-done();
-}
+    maxConnections : 2,
+    //rateLimit: 1000, // `maxConnections` will be forced to 1
+    userAgent: 'Googlebot/2.1 (+http://www.googlebot.com/bot.html)',
+    // This will be called for each crawled page
+    callback : function (error, res, done) {
+        if(error){
+            console.log(error);
+        }else{
+            //a lean implementation of core jQuery designed specifically for the server
+            console.log(res.options.uri);
+        }
+        done();
+    }
 });
-sitemaps.parseSitemaps(urls, function(url) { 
-c.queue(url);
+
+let date_ob = new Date();
+
+sitemaps.parseSitemaps(urls, (url) => { 
+    // Split url
+    let splittedUrl = url.split('/');
+
+    if (splittedUrl[4] == 'products') {
+
+        try {
+            let productId = parseInt(splittedUrl[6]);
+            let currentDay = date_ob.getDate();
+
+            // Check modulo of url by 26 and current day
+            let moduloResult = productId % 26;
+
+            if (moduloResult == currentDay) {
+                c.queue(url);
+            }
+        }
+        catch (ex) {
+            console.log("Error in url: " + url);
+        }
+
+    } else {
+        c.queue(url);
+    }
+
 }, function(err, sitemaps) {
-// start crawler
+    // start crawler
 });
+
 console.log("Start crawler");
